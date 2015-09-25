37afd593c15073f5f791e139c6b34bae033bdecc
Frederic LeBel <frederic.lebel@gmail.com>
2015-09-25 15:44:07 -0400

    Fix for bar when no music is queued.

--- scripts/bar_fragments.sh
+++ scripts/bar_fragments.sh
@@ -41,7 +41,7 @@ MusicStatus() {
   music_name="$(mpc current)"
   music_progress="$(mpc | head -n 2 | tail -n 1 | sed 's/%/%%/g')"
   music_state="$(mpc | head -n 2 | tail -n 1 | cut -d '[' -f2 | cut -d ']' -f1)"
-  if true; then
+  if [ -n "$music_name" ]; then
     template="%{A:mpc prev:}%{F#00FFFF} <|%{F-}%{A}"
     template="$template %{A:mpc toggle:}"
     if [ $music_state == "playing" ]; then
@@ -50,6 +50,8 @@ MusicStatus() {
       template="$template %{F#888888}$music_name%{F-} | %{F#888888}$music_progress%{F-}"
     fi
     template="$template %{A}%{A:mpc next:}%{F#00FFFF} |> %{F-}%{A} "
+  else
+    template="%{F#808080}no music%{F-}"
   fi
   echo $template
 }
